29102f691df43975abf2d9081ad20e122f8ca600
matentzn <nicolas.matentzoglu@gmail.com>
2019-09-20 14:25:45 +0100

    Update process.sh

--- process.sh
+++ process.sh
@@ -2,7 +2,10 @@
 
 set -e
 
-echo "Validate KB"
+echo "process started"
+echo "Start: vfb-pipeline-validatekb"
+echo "VFBTIME:"
+date
 
 echo 'START' >> ${WORKSPACE}/tick.out
 ## tail -f ${WORKSPACE}/tick.out >&1 &>&1
@@ -15,11 +18,14 @@ cd ..
 
 SCRIPTS=${WORKSPACE}/VFB_neo4j/src/uk/ac/ebi/vfb/neo4j/
 
-echo ''
-echo -e "travis_fold:start:neo4j_kb_validateschema"
 echo '** Transform old KB according to new schema **'
-export BUILD_OUTPUT=${WORKSPACE}/KBValidate.out
-${WORKSPACE}/runsilent.sh "python3 ${SCRIPTS}neo4j_kb_validateschema.py ${KBserver} ${KBuser} ${KBpassword}"
-cp $BUILD_OUTPUT /logs/
-egrep 'Exception|Error|error|exception|warning' $BUILD_OUTPUT
-echo -e "travis_fold:end:neo4j_kb_validateschema"
+#export BUILD_OUTPUT=${WORKSPACE}/KBValidate.out
+python3 ${SCRIPTS}neo4j_kb_validateschema.py ${KBserver} ${KBuser} ${KBpassword}
+#cp $BUILD_OUTPUT /logs/
+#egrep 'Exception|Error|error|exception|warning' $BUILD_OUTPUT
+
+
+echo "End: vfb-pipeline-validatekb"
+echo "VFBTIME:"
+date
+echo "process complete"
